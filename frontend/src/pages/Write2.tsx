6fb36e64025e43e02605949922b551f3d5579ec7
Hussain Ahmed Shaikh <hussainahmed.s.shaikh@gmail.com>
2025-03-12 23:53:27 +0000

    Update Write2.tsx

--- frontend/src/pages/Write2.tsx
+++ frontend/src/pages/Write2.tsx
@@ -143,7 +143,7 @@ export const Editor = () => {
 
   const editorRef = useRef<HTMLDivElement>(null);
 
-  // ----change made right here
+  // ----change made right here right
 
   useEffect(() => {
     const handleInput = () => {
