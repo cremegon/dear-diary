831a62efd1c8e3f02a5fa1fd234bb6ae0e0564ca
Hussain Ahmed Shaikh <hussainahmed.s.shaikh@gmail.com>
2025-03-25 13:51:04 +0000

    fix: verification and redirection successful

--- backend/src/middleware/validateToken.ts
+++ backend/src/middleware/validateToken.ts
@@ -21,23 +21,32 @@ export const validateToken = async (
   }
 
   // if user exists ; decode the token they have
-  const decoded = jwt.verify(token, JWT_SECRET) as jwt.JwtPayload;
-  console.log("decoded token from verification: ", decoded);
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as jwt.JwtPayload;
 
-  // select the user connected to the token from the database
-  const selectedUser = await pool.query(
-    "SELECT email FROM users WHERE id = $1",
-    [decoded.userId]
-  );
-  const user = selectedUser.rows[0];
+    // select the user connected to the token from the database
+    const selectedUser = await pool.query(
+      "SELECT email FROM users WHERE id = $1",
+      [decoded.userId]
+    );
+    const user = selectedUser.rows[0];
 
-  // if the user from the token does not match the user in the database, return False
-  if (!user.email === decoded.email) {
-    return res.status(400).json({ message: "Verficiation Failed" });
+    // if the user from the token does not match the user in the database, return False
+    if (!user.email === decoded.email) {
+      return res.status(400).json({ message: "Verficiation Failed" });
+    }
+    // if the user matches, then the verification is successful
+    return res.status(200).json({
+      message: "Verification Successful, redirecting...",
+      content: user.name,
+    });
+  } catch (error) {
+    if (error === "TokenExpiredError") {
+      console.log("Token has Expired");
+      return res.status(403).json({ message: "Token has Expired" });
+    } else {
+      console.log("Invalid Token");
+      return res.status(403).json({ message: "Invalid Token" });
+    }
   }
-  // if the user matches, then the verification is successful
-  return res.status(200).json({
-    message: "Verification Successful, redirecting...",
-    content: user.name,
-  });
 };
