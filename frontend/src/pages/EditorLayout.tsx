df90da64d03c415f3f8f2e0e6f934c49b8e099de
Hussain Ahmed Shaikh <hussainahmed.s.shaikh@gmail.com>
2025-04-04 10:27:38 +0100

    Update EditorLayout.tsx

--- frontend/src/pages/EditorLayout.tsx
+++ frontend/src/pages/EditorLayout.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar.tsx";
 
 export const EditorPageLayout = () => {
   return (
-    <div className="min-h-screen justify-center items-center my-20">
+    <div className="min-h-screen justify-center items-center">
+      <Navbar />
       <Outlet />
     </div>
   );
