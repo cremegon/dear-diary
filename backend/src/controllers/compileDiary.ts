69ab2374c0e5cb05aed13fec4a2c9fc8a6570eba
Hussain Ahmed Shaikh <hussainahmed.s.shaikh@gmail.com>
2025-04-16 13:23:18 +0100

    add: puppeteer to turn html to pdf

--- backend/src/controllers/compileDiary.ts
+++ backend/src/controllers/compileDiary.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { Pool } from "pg";
 import { config } from "../config";
-import { decryptUserId } from "../utils/security";
+import { JSDOM } from "jsdom";
 
 const pool = new Pool(config.db);
 
@@ -19,9 +19,33 @@ export const compileDiary = async (
   if (query.rows.length < 1)
     return res.status(404).json({ message: "No Cover Art Found Found" });
   const data = query.rows;
-  console.log(data);
+
+  const DOM = new JSDOM(
+    `<!DOCTYPE html><html><head><title>PDF Document</title></head><body></body></html>`
+  );
+  const document = DOM.window.document;
+
+  for (const {
+    id,
+    diary_id,
+    title,
+    created_at,
+    content,
+    font_family,
+    font_size,
+    url,
+  } of data) {
+    const h1 = document.createElement("h1");
+    const div = document.createElement("div");
+    h1.innerHTML = title;
+    div.innerHTML = content;
+    document.body.appendChild(h1);
+    document.body.appendChild(div);
+  }
+  console.log(document.body.innerHTML);
 
   return res.status(200).json({
     data: data,
+    message: "Found All Relevant Chapters in Diary",
   });
 };
