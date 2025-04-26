9e61e9d3abcea900b57db7d4a4fcc85e55633578
Hussain Ahmed Shaikh <hussainahmed.s.shaikh@gmail.com>
2025-04-26 20:26:40 +0100

    Update sendEmail.ts

--- backend/src/controllers/sendEmail.ts
+++ backend/src/controllers/sendEmail.ts
@@ -7,15 +7,17 @@ import { createTransport } from "nodemailer";
 const pool = new Pool(config.db);
 const serverEmail = config.serverEmail;
 const serverPassword = config.serverPassword;
+const senderEmail = config.senderEmail;
 
 export const send_PDF_Email = async (
-  req: Request,
-  res: Response
+  receipientEmail: string,
+  title: string,
+  author: string,
+  pdfBuffer: Buffer
 ): Promise<any> => {
   console.log("testing email...");
 
   const token = randomBytes(16).toString("hex").slice(0, 6);
-  const userEmail = "hussainahmed.s.shaikh@gmail.com";
 
   const transporter = createTransport({
     host: "smtp-relay.brevo.com",
@@ -26,10 +28,16 @@ export const send_PDF_Email = async (
   });
 
   const mailOptions = {
-    from: "hussain.artist.ahmed@gmail.com",
-    to: userEmail,
+    from: senderEmail,
+    to: receipientEmail,
     subject: "Hello from the Backend 2",
-    text: `<p>this is our second point of contact brother...${token}</p>`,
+    html: `<p>this is our second point of contact brother...${token}</p>`,
+    attachments: [
+      {
+        filename: `${title}-${author}.pdf`,
+        content: pdfBuffer,
+      },
+    ],
   };
 
   try {
@@ -37,7 +45,8 @@ export const send_PDF_Email = async (
     console.log("send successful");
   } catch (error) {
     console.log("error", error);
+    return false;
   }
 
-  return res.status(200).json({ message: "We in This" });
+  return true;
 };
