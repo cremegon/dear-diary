b1b0e42710e3b587c4547b3b2f2aaf5cb9f5e083
Hussain Ahmed Shaikh <hussainahmed.s.shaikh@gmail.com>
2025-05-09 19:24:15 +0100

    Update fetchTrustees.ts

--- backend/src/controllers/fetchTrustees.ts
+++ backend/src/controllers/fetchTrustees.ts
@@ -4,16 +4,34 @@ import { config } from "../config";
 
 const pool = new Pool(config.db);
 
+type relatedToTrustees = { [key: number]: any[] };
+
 export const fetchTrustees = async (
   req: Request,
   res: Response
 ): Promise<any> => {
   console.log("fetching trustees...");
 
-  const diaryEntry = await pool.query("SELECT * FROM trustees");
+  const trustees = await pool.query("SELECT * FROM trustees");
+
+  const diaryToTrustees: relatedToTrustees = {};
+  for (let i = 0; i < trustees.rows.length; i++) {
+    const diary_id = trustees.rows[i].diary_id;
+    const relatedDiaries = await pool.query(
+      "SELECT * FROM diaries WHERE id = $1",
+      [diary_id]
+    );
+    if (relatedDiaries && relatedDiaries.rows.length > 0) {
+      if (!diaryToTrustees[diary_id]) {
+        diaryToTrustees[diary_id] = [];
+      }
+      diaryToTrustees[diary_id].push(...relatedDiaries.rows);
+    }
+  }
 
   return res.status(200).json({
-    message: "Trustees fOUND",
-    data: diaryEntry.rows,
+    message: "Trustees Found",
+    data: trustees.rows,
+    diaries: diaryToTrustees,
   });
 };
