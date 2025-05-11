d323d84119be7507cc1eda9f4b4ac774b5c8c29d
Hussain Ahmed Shaikh <hussainahmed.s.shaikh@gmail.com>
2025-05-11 09:27:45 +0100

    add: fix multiple diaries link

--- backend/src/controllers/fetchTrustees.ts
+++ backend/src/controllers/fetchTrustees.ts
@@ -12,28 +12,32 @@ export const fetchTrustees = async (
 ): Promise<any> => {
   console.log("fetching trustees...");
 
+  //   const distinctTrustees = await pool.query()
+  const uniqueTrustees = await pool.query(
+    "SELECT DISTINCT name FROM trustees ORDER BY name ASC"
+  );
   const trustees = await pool.query("SELECT * FROM trustees");
 
-  const diaryToTrustees: relatedToTrustees = {};
-  const seen: string[] = [];
+  const diary_to_trustees: relatedToTrustees = {};
+
   for (let i = 0; i < trustees.rows.length; i++) {
     const diary_id = trustees.rows[i].diary_id;
-    const relatedDiaries = await pool.query(
-      "SELECT * FROM diaries WHERE id = $1",
-      [diary_id]
-    );
-    if (relatedDiaries && relatedDiaries.rows.length > 0) {
-      if (!diaryToTrustees[diary_id]) {
-        diaryToTrustees[diary_id] = [];
-      }
-      diaryToTrustees[diary_id].push(...relatedDiaries.rows);
+    const title = trustees.rows[i].title;
+
+    if (!diary_to_trustees[diary_id]) {
+      diary_to_trustees[diary_id] = [];
+    }
+    if (!diary_to_trustees[diary_id].includes(title)) {
+      console.log("curr title", title);
+      diary_to_trustees[diary_id].push(title);
     }
   }
-  console.log(diaryToTrustees);
+
+  console.log(diary_to_trustees);
 
   return res.status(200).json({
     message: "Trustees Found",
-    data: trustees.rows,
-    diaries: diaryToTrustees,
+    data: uniqueTrustees.rows,
+    diaries: diary_to_trustees,
   });
 };
