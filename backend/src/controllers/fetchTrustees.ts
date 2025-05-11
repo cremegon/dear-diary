55d11a880aa17e2a74d44c924e80792dce789d20
Hussain Ahmed Shaikh <hussainahmed.s.shaikh@gmail.com>
2025-05-11 10:39:10 +0100

    Update fetchTrustees.ts

--- backend/src/controllers/fetchTrustees.ts
+++ backend/src/controllers/fetchTrustees.ts
@@ -15,7 +15,7 @@ export const fetchTrustees = async (
   //   const distinctTrustees = await pool.query()
   const diaries = await pool.query("SELECT * FROM diaries;");
   const trustees = await pool.query(
-    "SELECT DISTINCT ON (name) * FROM trustees ORDER BY COUNT(name);"
+    "SELECT DISTINCT ON (name) * FROM trustees ORDER BY name ASC;"
   );
 
   const diary_to_trustees: relatedToTrustees = {};
