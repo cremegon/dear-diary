8dff7fcb404090882c33761a8bbdf395f3c8a2fc
Hussain Ahmed Shaikh <hussainahmed.s.shaikh@gmail.com>
2025-05-19 19:17:26 +0100

    Update authMiddleware.ts

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -1,1 +1,12 @@
 const verifyToken = require("./validateToken");
+
+function authMiddleware(req,res,next){
+    const token = req.cookies.authToken
+
+    if (!token) { 
+        return res.status(401).json({message: "NO Token present"})
+    }
+
+    const result = verifyToken(token)
+    if (!)
+}
