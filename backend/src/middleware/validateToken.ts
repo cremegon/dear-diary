3c0f708fdafe2c6f73b183f4e60b51eafda7b12a
Hussain Ahmed Shaikh <hussainahmed.s.shaikh@gmail.com>
2025-05-19 07:33:43 +0100

    Update validateToken.ts

--- backend/src/middleware/validateToken.ts
+++ backend/src/middleware/validateToken.ts
@@ -14,7 +14,7 @@ export const validateToken = async (
 
   const token = req.cookies.authToken;
   console.log(req.cookies, "hey");
-  // new feature: set up a gateway process
+  // new feature: set up a gateway process in order to
   // check if a token exists ; if not then a user isn't verified
   if (!token) {
     console.log("no tokens found at verification");
