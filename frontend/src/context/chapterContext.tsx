7a6e0423fe8054686fd68e326c381897afcd568d
Hussain Ahmed Shaikh <hussainahmed.s.shaikh@gmail.com>
2025-05-24 14:25:00 +0100

    Update chapterContext.tsx

--- frontend/src/context/chapterContext.tsx
+++ frontend/src/context/chapterContext.tsx
@@ -1,11 +1,13 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 const chapContext = createContext(null);
 
 const chapterContext = ({ children }) => {
-  const chapterArray = [];
+  const [chapterArray, setChapterArray] = useState([]);
 
   return (
-    <chapContext.Provider value={chapterArray}>{children}</chapContext.Provider>
+    <chapContext.Provider value={{ chapterArray, setChapterArray }}>
+      {children}
+    </chapContext.Provider>
   );
 };
