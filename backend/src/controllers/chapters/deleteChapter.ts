f0e1ac9472ab6f410686b712086a3fd055087cf0
Hussain Ahmed Shaikh <hussainahmed.s.shaikh@gmail.com>
2025-05-30 08:49:29 +0100

    add: chapter delete logic adjust

--- backend/src/controllers/chapters/deleteChapter.ts
+++ backend/src/controllers/chapters/deleteChapter.ts
@@ -18,8 +18,27 @@ export const deleteChapter = async (
     return res.status(404).send();
   }
 
+  let prevId = null;
+  let nextId = null;
+
+  const id = await pool.query("SELECT id FROM chapters WHERE url = $1", [
+    chapterURL,
+  ]);
+  const currentId = id.rows[0].id;
+  const chapterIdBefore = await pool.query(
+    "SELECT id FROM chapters WHERE id < $1 ORDER BY created_at DESC LIMIT 1",
+    [currentId]
+  );
+  const chapterIdAfter = await pool.query(
+    "SELECT id FROM chapters WHERE id > $1 ORDER BY created_at DESC LIMIT 1",
+    [currentId]
+  );
+
+  if (chapterIdBefore.rows[0])
+    console.log(chapterIdBefore.rows[0], chapterIdAfter.rows[0]);
+
   try {
-    await pool.query("DELETE FROM chapters WHERE url = $1", [chapterURL]);
+    // await pool.query("DELETE FROM chapters WHERE url = $1", [chapterURL]);
   } catch (error) {
     console.log("No Relevant Chapter Found");
     return res.status(404).send();
