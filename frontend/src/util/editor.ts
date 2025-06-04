b613cedf2b88c4ae3e05b1b9eb00effa63af1a48
cremegon <hussainahmed.s.shaikh@gmail.com>
2025-06-04 17:54:25 +0100

    Update editor.ts

--- frontend/src/util/editor.ts
+++ frontend/src/util/editor.ts
@@ -811,7 +811,7 @@ export function siblingMergeAfterBackspace() {
   const div_node = span?.parentNode;
   console.log("neighbouring span => ", span, next_span, next_span?.nodeName);
 
-  // For Browser Compatibility:
+  // For Browser Compatibility: If the browser sees
   if (div_node && span && next_span && next_span?.nodeName === "SPAN") {
     console.log("importing...");
     while (next_span.firstChild) {
