56d4b5891c00856a658012fc8315ba13dc26fa72
Hussain Ahmed Shaikh <hussainahmed.s.shaikh@gmail.com>
2025-06-13 14:45:24 +0100

    add: setting page

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export const SettingsPage = () => {
   return (
-    <div>
-      <h1 className="text-7xl">THIS IS THE SETTINGS PAGE!!!!</h1>
+    <div className="min-h-screen">
+      <h1 className="text-3xl font-bold">Settins</h1>
     </div>
   );
 };
