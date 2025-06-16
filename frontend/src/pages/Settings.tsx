361cf49c7ecb30aee416f97d1d3eeb0e6c1ac642
Hussain Ahmed Shaikh <hussainahmed.s.shaikh@gmail.com>
2025-06-16 19:03:24 +0100

    add: settings page

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+export const settingsArray = ["Change Password", "Confirm Deceased"];
 export const SettingsPage = () => {
   return (
     <div className="min-h-screen">
-      <h1 className="text-3xl font-bold">Settings</h1>
+      <h1 className="text-3xl font-bold mt-4 ml-4">Settings</h1>
+      <div></div>
     </div>
   );
 };
