b17c1a28266189a281f1541a2eabfaae7e14192a
Hussain Ahmed Shaikh <hussainahmed.s.shaikh@gmail.com>
2025-06-17 13:09:39 +0100

    Update Profile.tsx

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 
 export const ProfilePage = () => {
-  const [edit, setEdit] = useState(false);
+  const [bio, setBio] = useState(false);
   const user = localStorage.getItem("user");
   if (!user) return;
   const username = JSON.parse(user).username
     ? JSON.parse(user).username
     : "Guest";
+
+  function handleEdit() {
+    if (bio) {
+      handleUpdateProfileBio();
+    }
+    setBio(!bio);
+  }
   return (
     <div className="min-h-screen flex flex-col items-center">
       <div className="w-1/3 h-full flex flex-col">
@@ -14,10 +21,10 @@ export const ProfilePage = () => {
         <div className="w-40 h-40 bg-gray-300 rounded-full self-center" />
         <h2 className="mt-6">About You</h2>
         <textarea
-          disabled={edit}
+          disabled={bio}
           className="border-2 border-black h-52 my-4"
         ></textarea>
-        <button onClick={() => setEdit(!edit)} className="btn-writeUI">
+        <button onClick={handleEdit} className="btn-writeUI">
           Edit
         </button>
       </div>
