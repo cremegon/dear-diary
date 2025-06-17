01f1a33003f7fc68fbaf93ea8a53c8367a9e3d97
Hussain Ahmed Shaikh <hussainahmed.s.shaikh@gmail.com>
2025-06-17 12:47:58 +0100

    Update Profile.tsx

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -11,8 +11,9 @@ export const ProfilePage = () => {
       <div className="w-1/3 h-full flex flex-col">
         <h1 className="my-8 text-4xl font-bold text-center">{username}</h1>
         <div className="w-40 h-40 bg-gray-300 rounded-full self-center" />
-        <h2 className="mt-6 mb-2">About You</h2>
-        <textarea className="border-2 border-black h-52"></textarea>
+        <h2 className="mt-6">About You</h2>
+        <textarea className="border-2 border-black h-52 my-4"></textarea>
+        <button className="btn-writeUI">Edit</button>
       </div>
     </div>
   );
