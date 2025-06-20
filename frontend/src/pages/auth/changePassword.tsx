ca439375a22c0d1583a723059649a167bf28410c
Hussain Ahmed Shaikh <hussainahmed.s.shaikh@gmail.com>
2025-06-20 13:11:01 +0100

    Update changePassword.tsx

--- frontend/src/pages/auth/changePassword.tsx
+++ frontend/src/pages/auth/changePassword.tsx
@@ -3,7 +3,24 @@ import React from "react";
 export const ChangePassPage = () => {
   return (
     <div className="min-h-screen">
-      <h1>Change Password</h1>
+      <div className="flex flex-col items-center">
+        <h1 className="text-4xl font-bold my-6">Change Password</h1>
+        <input
+          type="text"
+          placeholder="current password"
+          className="mt-2 border-4 border-pink-400"
+        />
+        <input
+          type="text"
+          placeholder="new password"
+          className="mt-2 border-4 border-pink-400"
+        />
+        <input
+          type="text"
+          placeholder="re-type new password"
+          className="mt-2 border-4 border-pink-400"
+        />
+      </div>
     </div>
   );
 };
