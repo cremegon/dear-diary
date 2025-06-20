92e83a2b083ca1b06b502614bbc7c9e0d2ae15fd
Hussain Ahmed Shaikh <hussainahmed.s.shaikh@gmail.com>
2025-06-20 16:28:15 +0100

    Update changePassword.tsx

--- frontend/src/pages/auth/changePassword.tsx
+++ frontend/src/pages/auth/changePassword.tsx
@@ -4,38 +4,46 @@ export const ChangePassPage = () => {
   const [current, setCurrent] = useState("");
   const [newPass, setNewPass] = useState("");
   const [reTypeNew, setReTypeNew] = useState("");
-  const [error, setError] = useState(false);
+  const [error, setError] = useState({ status: false, message: "" });
 
   const currentRef = useRef<HTMLInputElement | null>(null);
   const newRef = useRef<HTMLInputElement | null>(null);
   const reTypeNewRef = useRef<HTMLInputElement | null>(null);
   return (
-    <div className="min-h-screen">
-      <div className="flex flex-col items-center">
+    <div className="min-h-screen flex flex-col items-center">
+      <div className="flex flex-col">
         <h1 className="text-4xl font-bold my-6">Change Password</h1>
+        <div className={`${error && error.status ? "block" : "hidden"}`}>
+          {error.message}
+        </div>
+        <h2 className="text-start mt-4">Enter Current Password</h2>
         <input
           ref={currentRef}
           type="text"
           placeholder="current password"
           value={current}
           onChange={(e) => setCurrent(e.target.value)}
-          className="mt-2 border-4 border-pink-400"
+          className="border-4 border-pink-400"
         />
+
+        <h2 className="text-start mt-4">Enter New Password</h2>
         <input
           ref={newRef}
           type="text"
           placeholder="new password"
           value={newPass}
           onChange={(e) => setNewPass(e.target.value)}
-          className="mt-2 border-4 border-pink-400"
+          className="border-4 border-pink-400"
         />
+
+        <h2 className="text-start mt-4">Re-Enter New Password</h2>
         <input
           ref={reTypeNewRef}
           type="text"
           placeholder="re-type new password"
           value={reTypeNew}
           onChange={(e) => setReTypeNew(e.target.value)}
-          className="my-2 border-4 border-pink-400"
+          className="mb-6 border-4 border-pink-400"
         />
         <button className="btn-writeUI">Confirm</button>
       </div>
