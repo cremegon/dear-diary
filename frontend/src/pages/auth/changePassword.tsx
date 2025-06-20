65e42262548123f9f411f086d24ea3e62d76763d
Hussain Ahmed Shaikh <hussainahmed.s.shaikh@gmail.com>
2025-06-20 13:12:53 +0100

    Update changePassword.tsx

--- frontend/src/pages/auth/changePassword.tsx
+++ frontend/src/pages/auth/changePassword.tsx
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 
 export const ChangePassPage = () => {
+  const currentRef = useRef<HTMLInputElement | null>(null);
+  const newRef = useRef<HTMLInputElement | null>(null);
+  const reTypeNewRef = useRef<HTMLInputElement | null>(null);
   return (
     <div className="min-h-screen">
       <div className="flex flex-col items-center">
         <h1 className="text-4xl font-bold my-6">Change Password</h1>
         <input
+          ref={currentRef}
           type="text"
           placeholder="current password"
           className="mt-2 border-4 border-pink-400"
         />
         <input
+          ref={newRef}
           type="text"
           placeholder="new password"
           className="mt-2 border-4 border-pink-400"
         />
         <input
+          ref={reTypeNewRef}
           type="text"
           placeholder="re-type new password"
           className="mt-2 border-4 border-pink-400"
