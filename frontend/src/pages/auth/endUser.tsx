7aaf134d2e612f3203b15d7fc28ea4e08217295f
Hussain Ahmed Shaikh <hussainahmed.s.shaikh@gmail.com>
2025-06-21 15:33:35 +0100

    Update endUser.tsx

--- frontend/src/pages/auth/endUser.tsx
+++ frontend/src/pages/auth/endUser.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 export const EndUser = () => {
-  return <div className="min-h-screen"></div>;
+  return (
+    <div className="min-h-screen">
+      <div>
+        <h1 className="text-4xl font-bold">End User History</h1>
+        <p>
+          This page exists to confirm the presence of the user's health,
+          well-being and activity. In order to continue the life
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default EndUser;
