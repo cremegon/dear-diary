7c57136593c6b42eb22d64f03df5066c25b14a0b
Hussain Ahmed Shaikh <hussainahmed.s.shaikh@gmail.com>
2025-06-23 20:44:10 +0100

    Update endUser.tsx

--- frontend/src/pages/auth/endUser.tsx
+++ frontend/src/pages/auth/endUser.tsx
@@ -5,7 +5,9 @@ export const EndUser = () => {
     <div className="min-h-screen flex flex-col items-center">
       <div className="w-1/2">
         <h1 className="text-4xl font-bold mt-8">End User History</h1>
-        <p className="mt-10">This page exists to confirm the action</p>
+        <p className="mt-10">
+          This page exists to confirm the action to be taken
+        </p>
       </div>
     </div>
   );
