dd5aba4e8c5af8974cee5c1711ee4cf26ad4ed00
Hussain Ahmed Shaikh <hussainahmed.s.shaikh@gmail.com>
2025-06-23 20:41:36 +0100

    Update endUser.tsx

--- frontend/src/pages/auth/endUser.tsx
+++ frontend/src/pages/auth/endUser.tsx
@@ -8,11 +8,7 @@ export const EndUser = () => {
         <p className="mt-10">
           This page exists to confirm the presence of the user's health,
           well-being and activity. In order to continue the life span of this
-          diary in parallel with the writer's presence, please answer the
-          security questions below. Failure to answer correctly within 3-months
-          will automatically end the life cycle of this diary and distribute
-          copies of all diaries to their designated trustees and permanently
-          conclude the life cycle of the writer's history
+          diary in parallel with the writer's presence, please upload
         </p>
       </div>
     </div>
