d309c7aee759bf918b2dba7e1d760026395c75ea
Hussain Ahmed Shaikh <hussainahmed.s.shaikh@gmail.com>
2025-07-01 14:15:31 +0100

    add: backend endpoint connections

---
+++ backend/src/controllers/diaries/diaryUploadRoute.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import { upload } from "./upload";
+import cors from "cors";
+import { uploadManualDiary } from "./uploadManualDiary";
+import cookieParser from "cookie-parser";
+
+const router = express.Router();
+router.use(cookieParser());
+router.use(
+  cors({
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: `http://localhost:3000`,
+    credentials: true,
+  })
+);
+router.use(cookieParser());
+router.post("/upload-manual-diary", upload.single("file"), uploadManualDiary);
+
+export default router;
--- backend/src/controllers/diaries/uploadManualDiary.ts
+++ backend/src/controllers/diaries/uploadManualDiary.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { Pool } from "pg";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { config } from "../../config";
+import { encryptUserId } from "../../utils/security";
 
 const pool = new Pool(config.db);
 const JWT_SECRET = config.jwtSecret;
@@ -15,11 +16,19 @@ export const uploadManualDiary = async (
   const token = req.cookies.authToken;
   const decoded = jwt.verify(token, JWT_SECRET as string) as JwtPayload;
   const userId = decoded.id;
+  const now = new Date();
+  const formattedDateNow = now.toISOString().replace("T", " ").slice(0, 23);
 
-  await pool.query("INSERT INTO diaries(user_id,title,pdf) VALUES ($1,$2,$3)", [
-    userId,
-    title,
-    filepath,
+  const query = await pool.query(
+    "INSERT INTO diaries(user_id,title,pdf,completed_at) VALUES ($1,$2,$3)",
+    [userId, title, filepath, formattedDateNow]
+  );
+  const id = query.rows[0].id;
+  const encryptedURL = encryptUserId(id);
+
+  await pool.query("UPDATE diaries SET url = $1 WHERE id = $2", [
+    encryptedURL,
+    id,
   ]);
 
   return res.status(200).json({
