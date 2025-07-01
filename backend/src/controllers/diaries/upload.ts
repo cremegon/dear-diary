7d579d5a9482d0573adea09d126003a688263185
Hussain Ahmed Shaikh <hussainahmed.s.shaikh@gmail.com>
2025-07-01 14:01:42 +0100

    add: backend for manual diary upload

---
+++ backend/src/controllers/diaries/upload.ts
@@ -0,0 +1,33 @@
+import multer from "multer";
+import { Request } from "express";
+import { config } from "../../config";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { v2 as cloudinary } from "cloudinary";
+import jwt, { JwtPayload } from "jsonwebtoken";
+const JWT_SECRET = config.jwtSecret;
+
+cloudinary.config({
+  cloud_name: config.cloudName,
+  api_key: config.apiKey,
+  api_secret: config.apiSecret,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: async (req: Request, file: Express.Multer.File) => {
+    const token = req.cookies.authToken;
+    const decoded = jwt.verify(token, JWT_SECRET as string) as JwtPayload;
+    const userId = decoded.id;
+
+    return {
+      folder: "user-manual-diaries",
+      format: "pdf", // supports promises as well
+      public_id: `${userId}` + "-" + Date.now(),
+    };
+  },
+});
+
+export const upload = multer({
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
