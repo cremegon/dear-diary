97cf6d49d4c96f85038b08f197e1f3e35b751e5e
Hussain Ahmed Shaikh <hussainahmed.s.shaikh@gmail.com>
2025-07-02 08:35:31 +0100

    Update diaryPDFBuffer.ts

---
+++ backend/src/controllers/diaries/diaryPDFBuffer.ts
@@ -0,0 +1,9 @@
+import { Readable } from "stream";
+
+export function bufferToStream(buffer: Buffer) {
+  const readable = new Readable();
+  readable._read = () => {};
+  readable.push(buffer);
+  readable.push(null);
+  return readable;
+}
